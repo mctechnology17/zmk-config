dcf7e1a1e39034a398576db314a795a6fbe7c3ff
Chow Castro, Marcos <mctechnology170318@gmail.com>
2022-09-19 15:33:07 +0200

    test

--- config/keymap_german_mac_iso.h
+++ config/keymap_german_mac_iso.h
@@ -15,9 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+// #pragma once
 
-#include <dt-bindings/zmk/keys.h>
+// #include <dt-bindings/zmk/keys.h>
 
 #define XXX &none
 #define ___ &trans
