e6267898049c3474acca5c785b2bd90fd3c76b80
Chow Castro, Marcos <mctechnology170318@gmail.com>
2022-09-22 21:05:21 +0200

    test

--- config/keymap_german_mac_iso.h
+++ config/keymap_german_mac_iso.h
@@ -1,29 +1,67 @@
 // Row 1
 // #define DE_CIRC &kp GRAVE // ^ (dead)
-#define DE_1 &kp N1 // 1
-#define DE_2 &kp N2 // 2
-#define DE_3 &kp N3 // 3
-#define DE_4 &kp N4 // 4
-#define DE_5 &kp N5 // 5
-#define DE_6 &kp N6 // 6
-#define DE_7 &kp N7 // 7
-#define DE_8 &kp N8 // 8
-#define DE_9 &kp N9 // 9
-#define DE_0 &kp N0 // 0
-// #define DE_SS &kp MINUS   // ß
-// #define DE_ACUT &kp EQUAL // ´ (dead)
+#define DE_1 &kp N1       // 1
+#define DE_2 &kp N2       // 2
+#define DE_3 &kp N3       // 3
+#define DE_4 &kp N4       // 4
+#define DE_5 &kp N5       // 5
+#define DE_6 &kp N6       // 6
+#define DE_7 &kp N7       // 7
+#define DE_8 &kp N8       // 8
+#define DE_9 &kp N9       // 9
+#define DE_0 &kp N0       // 0
+#define DE_SS &kp MINUS   // ß
+#define DE_ACUT &kp EQUAL // ´ (dead)
+// Row 2
+#define DE_Q &kp Q       // Q
+#define DE_W &kp W       // W
+#define DE_E &kp E       // E
+#define DE_R &kp R       // R
+#define DE_T &kp T       // T
+#define DE_Z &kp Y       // Z
+#define DE_U &kp U       // U
+#define DE_I &kp I       // I
+#define DE_O &kp O       // O
+#define DE_P &kp P       // P
+#define DE_UDIA &kp LBKT // Ü
+#define DE_PLUS &kp RBKT // +
+// Row 3
+#define DE_A &kp A            // A
+#define DE_S &kp S            // S
+#define DE_D &kp D            // D
+#define DE_F &kp F            // F
+#define DE_G &kp G            // G
+#define DE_H &kp H            // H
+#define DE_J &kp J            // J
+#define DE_K &kp K            // K
+#define DE_L &kp L            // L
+#define DE_ODIA &kp SEMICOLON // Ö
+#define DE_ADIA &kp APOS      // Ä
+#define DE_HASH &kp BSLH      // #
+// Row 4
+#define DE_LABK &kp GRAVE // <
+#define DE_Y &kp Z        // Y
+#define DE_X &kp X        // X
+#define DE_C &kp C        // C
+#define DE_V &kp V        // V
+#define DE_B &kp B        // B
+#define DE_N &kp N        // N
+#define DE_M &kp M        // M
+#define DE_COMM &kp COMMA // ,
+#define DE_DOT &kp DOT    // .
+#define DE_MINS &kp SLASH // -
 
-#define DE_Y &kp Z                // Y
-#define DE_Z &kp Y                // Z
-#define DE_SS &kp MINUS           // ß
-#define DE_UDIA &kp LBKT          // Ü
-#define DE_ODIA &kp SEMICOLON     // Ö
-#define DE_ADIA &kp APOS          // Ä
-#define DE_MINS &kp SLASH         // -
-#define DE_FSLH &kp AMPS          // "/"
-#define DE_STAR &kp RBRC          // "*"
-#define DE_EQUAL &kp RPAR         // =
-#define DE_PLUS &kp RBKT          // +
+#define DE_Y &kp Z      // Y
+#define DE_Z &kp Y      // Z
+#define DE_SS &kp MINUS // ß
+// #define DE_UDIA &kp LBKT          // Ü
+// #define DE_ODIA &kp SEMICOLON     // Ö
+// #define DE_ADIA &kp APOS          // Ä
+#define DE_MINS &kp SLASH // -
+#define DE_FSLH &kp AMPS  // "/"
+#define DE_STAR &kp RBRC  // "*"
+#define DE_EQUAL &kp RPAR // =
+// #define DE_PLUS &kp RBKT          // +
 #define DE_GRAVE &kp PLUS         // `
 #define DE_HASH &kp BSLH          // #
 #define DE_TILDE &kp LA(RBKT)     // ~
@@ -38,9 +76,9 @@
 #define DE_RBKT &kp RA(N9)        // ]
 #define DE_LBRC &kp RA(N7)        // {
 #define DE_RBRC &kp RA(N0)        // }
-#define DE_LABK &kp GRAVE         // <
-#define DE_RABK &kp LS(GRAVE)     // >
-#define DE_APOS &kp PIPE          // '
-#define DE_ACUT &kp EQUAL         // ´ hasta aqui funciona
+// #define DE_LABK &kp GRAVE         // <
+#define DE_RABK &kp LS(GRAVE) // >
+#define DE_APOS &kp PIPE      // '
+// #define DE_ACUT &kp EQUAL         // ´ hasta aqui funciona
 
 #define DE_CARET &kp HASH // ^ TODO:
