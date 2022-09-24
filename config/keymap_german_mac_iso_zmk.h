3dc52fcd8e423babb4c789604938f9496acc7ceb
Chow Castro, Marcos <mctechnology170318@gmail.com>
2022-09-24 16:54:44 +0200

    test re def

--- config/keymap_german_mac_iso_zmk.h
+++ config/keymap_german_mac_iso_zmk.h
@@ -348,24 +348,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DE_MDSH &kp LS(LA(MINUS)) // —
 
 // ###############
-#define DE_FSLH &kp AMPS  // "/"
-#define DE_STAR &kp RBRC  // "*"
-#define DE_EQUAL &kp RPAR // =
-#define DE_GRAVE &kp PLUS         // `
+// #define DE_FSLH &kp AMPS  // "/" DE_SLSH
+// #define DE_STAR &kp RBRC  // "*" DE_ASTR
+// #define DE_EQUAL &kp RPAR // = DE_EQL
+// #define DE_GRAVE &kp PLUS         // ` DE_GRV
 // #define DE_HASH &kp BSLH          // #
 // #define DE_TILDE &kp LA(RBKT)     // ~
 // #define DE_PIPE &kp RA(GRAVE)     // "|"
-#define DE_AMPS &kp CARET         // "&"
-#define DE_BSLH &kp RA(MINUS)     // "\"
-#define DE_QUESTION &kp LS(MINUS) // ?
+#define DE_AMPS &kp CARET         // "&" DE_AMPR
+#define DE_BSLH &kp RA(MINUS)     // "\" DE_BSLS
+#define DE_QUESTION &kp LS(MINUS) // ? DE_QUES
 // #define DE_AT &kp RA(Q)           // "@"
-#define DE_LPAR &kp LS(N8)        // (
-#define DE_RPAR &kp LS(N9)        // )
-#define DE_LBKT &kp RA(N8)        // [
-#define DE_RBKT &kp RA(N9)        // ]
-// #define DE_LBRC &kp RA(N7)        // {
-// #define DE_RBRC &kp RA(N0)        // }
-#define DE_APOS &kp PIPE      // '
+#define DE_LPAR &kp LS(N8)        // ( DE_LPRN
+#define DE_RPAR &kp LS(N9)        // ) DE_RPRN
+#define DE_LBKT &kp RA(N8)        // [ DE_LBRC
+#define DE_RBKT &kp RA(N9)        // ] DE_RBRC
+// #define DE_LBRC &kp RA(N7)        // { DE_LCBR
+// #define DE_RBRC &kp RA(N0)        // } DE_RCBR
+#define DE_APOS &kp PIPE      // ' DE_QUOT
 
-#define DE_CARET &kp HASH // ^ TODO:
+#define DE_CARET &kp HASH // ^ TODO: DE_CIRC
 // vim: fdm=marker:
